0f8f2f52609f350372ec99781a7dcd206ec7e26d
jasoncast880 <jasoncast880@gmail.com>
2024-04-18 13:15:12 -0400

    Https Endpoint established

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,44 @@
 const functions = require('firebase-functions');
-const Alexa = require('ask-sdk-core');
 
 const express = require('express');
+const { SkillBuilders } = require('ask-sdk-core');
 const {ExpressAdapter} = require('ask-sdk-express-adapter');
 
 //ask handlers here
 const app = express();
-const skillBuilder = Alexa.SkillBuilders.custom();
+
+const skillBuilder = SkillBuilders.custom();
+
+const GetNewFactIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'GetNewFactIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'hello joyce im watching you!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Define your intent handlers here
+// ...
+
+// Add intent handlers to the skill builder
+skillBuilder.addRequestHandlers(
+    // Your intent handlers here
+    GetNewFactIntentHandler
+);
+
 const skill = skillBuilder.create();
-const adapter = newExpressAdapter(skill, true, true);
+
+const adapter = new ExpressAdapter(skill, true, true);
 
 app.post('/', adapter.getRequestHandlers());
-app.listen(3000);
 
+// Export the Express app for Firebase Cloud Functions
+exports.alexaskill = functions.https.onRequest(app);
 
-exports.alexskill = functions.https.onRequest((req, response) => {
-  if(!skillBuilder) {
-    skillBuilder = Alexa.SkillBuilders.custom()
-      .addRequestHandlers(
-        LaunchRequestHandler,
-        ExitHandler,
-        SessionEndedRequestHandler
-      )
-      .addErrorHandlers(ErrorHandler)
-      .create();
-
-    //possibly do validation on signatures here
-    
-    //invoke the skill
-    const responseASK = skill.invoke(req.body);
-    response.send(responseASK.response);
-    
-});
