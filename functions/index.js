f4c0f4989bd843c4b11ac7ce9f786da1d4cc8eab
jasoncast880 <jasoncast880@gmail.com>
2024-04-18 17:48:12 -0400

    it works???

--- functions/index.js
+++ functions/index.js
@@ -3,98 +3,52 @@ const express = require('express');
 const { ExpressAdapter  } = require('ask-sdk-express-adapter');
 const Alexa = require('ask-sdk-core');
 
-//loging interceptors
-const LogRequestInterceptor = {
-  process(handlerInput) {
-    //Log Req
-    console.log("=REQUEST=");
-    console.log(JSON.stringify(handlerInput.reqestEnvelope, null, 2));
-  }
-}
+exports.alexaSkill = functions.https.onRequest((request, response) => {
+  const type = JSON.stringify(request.body.request.type);
+  const name = JSON.stringify(request.body.request.intent.name);
+  
+  const result = getAlexaResponse(type, name);
 
-const LogResponseInterceptor = {
-  process(handlerInput, response) {
-    //Log Req
-    console.log("=RESPONSE=");
-    console.log(JSON.stringify(response, null, 2));
-  }
-}
+  response.send(result);
+});
 
-const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    Alexa.getIntentName(handlerInput.requestEnvelope) === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speakOutput = 'Welcome to my skill, Hello World';
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .getResponse();
-  }
-};
-
-const GeneralIntentHandler = {
-  canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-  },
-  handle(handlerInput) {
-    const speakOutput = 'Help me';
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .getResponse();
-  }
-};
-
-const WeatherIntentHandler = {
-  canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-    && Alexa.getIntentName(handlerInput.requestEnvelope) === 'WeatherIntent';
-  },
-  handle(handlerInput) {
-    const speakOutput = 'Todays weather is kinda hi.';
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .getResponse();
-  }
-};
-
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest'
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder.getResponse();
-  }
-};
-
-const ErrorHandler = {
-    canHandle() {
-        return true;
+const getAlexaResponse = (type, name) => {  
+  var AlexaDefaultAnswer = {
+    "version": "1.0",
+    "response": {
+      "outputSpeech": {
+        "type": "SSML",
+        "ssml": "<speak>Welcome to the Alexa Skills Kit, you can say hello!</speak>"
+      },
+      "shouldEndSession": false,
+      "card": {
+        "type": "Simple",
+        "title": "LaunchRequest",
+        "content": "Welcome to the Alexa Skills Kit, you can say hello!"
+      }
     },
-    handle(handlerInput, error) {
-        console.log(`Error handled: ${error.message}`);
-        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+    "userAgent": "ask-node/2.3.0 Node/v8.10.0",
+    "sessionAttributes": {}
+  }
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
+  if(type == '"LaunchRequest"') {
+      return AlexaDefaultAnswer;
+  } else if(type == '"IntentRequest"' && name =='"GetCurrentTimeIntent"'){
+      AlexaDefaultAnswer.response.outputSpeech.ssml = "<speak>" + currentTime() + "</speak>";
+      AlexaDefaultAnswer.response.card.content = currentTime();
+      return AlexaDefaultAnswer;
+  } else {
+    return AlexaDefaultAnswer;
+  }
+
 };
 
-const app = express();
-const skillBuilder = Alexa.SkillBuilders.custom();
-const skill = skillBuilder.addRequestHandlers(
-    LaunchRequestHandler,
-    GeneralIntentHandler,
-    WeatherIntentHandler,
-    SessionEndedRequestHandler)
-  .addErrorHandlers(ErrorHandler)
-  .addRequestInterceptors(LogRequestInterceptor)
-  .addResponseInterceptors(LogResponseInterceptor)
-  .create();
-const adapter = new ExpressAdapter(skill, true, true);
+/**********************************************************************************/
+/***********************************ANSWERS****************************************/
+/************************************************+++++++++++++++++++++++++++++++++*/
 
-app.get('/', adapter.getRequestHandlers());
-
-// Define and export the Firebase Cloud Function
-exports.myFirebaseFunction = functions.https.onRequest(app);
+function currentTime(){
+    const date = new Date();
+    //Return time in UTC !!!
+    return date.getHours() + ":" + date.getMinutes();
+};
